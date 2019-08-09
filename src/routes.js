eb3545e07778e8bd81d7c0ebdd55107b522b8f2c
PatrickGmailHub <pgo.guedes@gmail.com>
2019-08-09 18:00:21 -0300

    Implementando o upload de arquivos

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,18 @@
 // const{ Router } = require('express');
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 // import User from './app/models/User';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -17,6 +21,10 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+// routes.post('/files', upload.single('file'), (req, res) => res.json({ ok: 'ok' }));
+// routes.post('/files', upload.single('file'), (req, res) => res.json(req.file));
+routes.post('/files', upload.single('file'), FileController.store);
 
 /* routes.get('/', async (req, res) => {
   const user = await User.create({
