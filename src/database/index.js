a2890802cfde42be2efd81240a6f10179c123406
PatrickGmailHub <pgo.guedes@gmail.com>
2019-08-13 17:16:44 -0300

    Migrate e Model de Agendamento

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,11 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
+import Agendamento from '../app/models/Agendamento';
 
 import databaseConfig from '../config/database';
 
-const models = [User, File];
+const models = [User, File, Agendamento];
 
 class Database {
   constructor() {
